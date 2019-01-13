d3ad4d3158249bb0d2fcf618cd4b0fd98493343e
Alaa-samir <38021201+Alaa-samir@users.noreply.github.com>
2019-01-13 16:20:11 +0400

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,3 +3,22 @@
 def test(name) {
   echo "Hello.${name}"
 }
+def build() {
+  // mvn clean deploy -U
+  echo 'build'
+}
+def test(name) {
+  // sh "/usr/local/bin/${name}"
+  echo '/usr/local/bin/${name}'
+}
+def deploy(env,app) {
+ // aws cloudformation create-stack \
+  //--stack-name ${env}_{app)
+  //--parameters \
+    //ParameterKey=Env,ParameterValue=${env}
+  //...(rest of the params go here)
+  echo 'stack-name ${env}_{app)'
+}
+def call(String name = 'human') {
+  echo "Hello, ${name}."
+}
