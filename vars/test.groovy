fee22576603f0e0d34440a3206e6e5f3e13ebf2f
Alaa-samir <38021201+Alaa-samir@users.noreply.github.com>
2019-01-13 14:40:32 +0400

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,18 +1,24 @@
-#!/usr/bin/groovy
-package io.abc;
+//src/io/abc/pipeline.groovy
+//#!/usr/bin/groovy
+//package io.abc;
 def build() {
-  mvn clean deploy -U
+  // mvn clean deploy -U
+  echo 'build'
 }
 def test(name) {
-  sh "/usr/local/bin/${name}"
+  // sh "/usr/local/bin/${name}"
+  echo '/usr/local/bin/${name}'
 }
 def deploy(env,app) {
-  aws cloudformation create-stack \
-  --stack-name ${env}_{app)
-  --parameters \
-    ParameterKey=Env,ParameterValue=${env}
-  ...(rest of the params go here)
+ // aws cloudformation create-stack \
+  //--stack-name ${env}_{app)
+  //--parameters \
+    //ParameterKey=Env,ParameterValue=${env}
+  //...(rest of the params go here)
+  echo 'stack-name ${env}_{app)'
+}
+def call(String name = 'human') {
+  echo "Hello, ${name}."
 }
 // AimTheory have a recommendation and explanation about this here
 return this
-Now 
