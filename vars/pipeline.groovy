432e4bc7a4a05137df1c4d9821740f43e7fecfbb
Alaa-samir <38021201+Alaa-samir@users.noreply.github.com>
2019-01-13 13:57:02 +0400

    Create pipeline.groovy

---
+++ vars/pipeline.groovy
@@ -0,0 +1,20 @@
+
+import com.cleverbuilder.pipline
+ //  println GlobalVars.foo
+ // instantiate
+def call(){ 
+pl = new pipline()
+// work
+stage ('1'){
+pl.build()
+//assert pl.deploy ('qa','api')
+pl.deploy ('qa','api')
+pl.test ( name: 'regression')
+pl.deploy('staging','api')
+pl.test('load')
+pl.deploy('prod','api')
+pl.test('Marge')                         
+//assert 'WonderMarge' ==  pl.test()
+sayHello('alaa')
+ // pipline(name: 'alaa')  
+}}
